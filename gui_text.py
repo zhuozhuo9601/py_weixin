421d92eb2a3528e7f94466cbd45a27dd74f93fc8
fanlizhuo <zhuozhuo9601@mail.com>
2020-01-17 14:55:14 +0800

    GUI编程增加登陆访问

--- gui_text.py
+++ gui_text.py
@@ -5,17 +5,15 @@ import sys
 
 import time
 
+import requests
+
 
 class tk_t(object):
     def __init__(self):
-        # 第1步，实例化object，建立窗口window
         window = tk.Tk()
         self.window = window
-        # 第2步，给窗口的可视化起名字
         window.title('My Window')
-        # 第3步，设定窗口的大小(长 * 宽)
-        window.geometry('500x300')  # 这里的乘是小x
-        # 第4步，在图形界面上创建 500 * 200 大小的画布并放置各种元素
+        window.geometry('500x300')
         canvas = tk.Canvas(window, bg='black', height=200, width=500)
         dir_path = os.getcwd()
         c_sys = sys.platform
@@ -32,34 +30,57 @@ class tk_t(object):
         username = tk.Entry(window, show=None, font=('Arial', 14), textvariable=v_name)
         password = tk.Entry(window, show='*', font=('Arial', 14), textvariable=v_word)
 
-        self.v_name=v_name
-        self.v_word=v_word
+        self.v_name = v_name
+        self.v_word = v_word
         username.pack()
         password.pack()
         b = tk.Button(window, text="登陆", command=self.user_value).place(x=170, y=260)
         b_out = tk.Button(window, text="退出", command=window.quit).place(x=280, y=260)
         window.mainloop()
+
     def user_value(self):
         user_name = self.v_name.get()
         user_word = self.v_word.get()
         user_dict = {
-            "username":user_name,
-            "pasword":user_word
+            "username": user_name,
+            "password": user_word
         }
         self.window.destroy()
         time.sleep(1)
-        chat_with()
-        return user_dict
+        res = user_login(user_dict)
+        if '200' in str(res):
+            chat_with()
+        return res
+
 
 class chat_with(object):
     def __init__(self):
-        # 第1步，实例化object，建立窗口window
         chat = tk.Tk()
-        # 第2步，给窗口的可视化起名字
         chat.title('My Window')
-        # 第3步，设定窗口的大小(长 * 宽)
-        chat.geometry('500x300')  # 这里的乘是小x
+        chat.geometry('500x300')
         tk.Label(chat, text='进入主界面了', bg='red', font=('Arial', 16)).pack()
         chat.mainloop()
 
-t = tk_t()
+
+def user_login(user_dict):
+    url = "http://www.zexclusive.top/login/"
+    data = {'username': user_dict['username'],
+            'password': user_dict['password']
+            }
+    headers = {
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9',
+        'Connection': 'keep-alive',
+        'Content-Length': '49',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Cookie': 'sessionid=q74c4gm9tng3nglitay95yqmxsux4dhp',
+        'Host': 'www.zexclusive.top',
+        'Referer': 'http://www.zexclusive.top/login/',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
+        'X-Requested-With': 'XMLHttpRequest'
+    }
+    res = requests.post(url=url, data=data, headers=headers, verify=False)
+    return res
+
+tk_t()
